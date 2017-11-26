2b7e507207a3defe6cbf374e4066bb374995835a
Peter <lawsonpd@gmail.com>
2017-11-26 14:58:47 -0600

    Add hook in validateNewUser to throw an error to block registration.

--- imports/startup/server/accounts-config.js
+++ imports/startup/server/accounts-config.js
@@ -4,3 +4,9 @@ Accounts.onCreateUser((options, user) => {
   user.role = 'driver';
   return user;
 });
+
+Accounts.validateNewUser((user) => {
+  if (true) {
+    throw new Meteor.Error(403, 'Registration error');
+  }
+});
